488948d28db7a41779695d02517f376d4b024376
Luis270100 <luisarthe27@gmail.com>
2019-09-16 22:55:32 -0500

    Heroku

--- server.js
+++ server.js
@@ -5,21 +5,21 @@ const path = require('path')
 const app = express()
 const api = require('./server/routes/api')
 
-app.use(express.static(path.join(__dirname,'/node_modules')))
-app.use(express.static(path.join(__dirname,'/dist')))
+app.use(express.static(path.join(__dirname, '/node_modules')))
+app.use(express.static(path.join(__dirname, '/dist')))
 
 
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-mongoose.connect("mongodb://localhost/Weather-App", {useNewUrlParser: false}, ()=> {console.log("connected")})
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Weather-App", { useNewUrlParser: false }, () => { console.log("connected") })
 app.use("/", api)
 
 
 
+const Port = 4000
 
-
-app.listen(4000, function(){
+app.listen(process.env.PORT || Port, function () {
     console.log("Server up and running on port 4000")
 })
 
